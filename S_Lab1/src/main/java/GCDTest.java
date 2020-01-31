a8e97ed05ce84d3b75b88fca141ac93cd3f8e860
1807132 <1807132@RG-N533-Q02.rgu.ac.uk>
2020-01-31 14:56:29 +0000

    Added starting point Semester 2 lab_1

---
+++ S_Lab1/src/main/java/GCDTest.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author 1807132
+ */
+public class GCDTest {
+    
+    
+    public static void main(String[] args) {
+        
+        //System.out.println(gcd(77,21));
+        System.out. println(gcdVerbose(77,21));
+    }
+    
+    public static int gcd(int m, int n) {
+        int p = m;
+        int q = n;
+        while (p % q != 0) {
+            int r = p % q;
+            p = q;
+            q = r;
+        }
+        return q;
+    }
+    
+    public static int gcdVerbose(int m, int n) {
+        int p = m;
+        int q = n;
+        while (p % q != 0) {
+            int r = p % q;
+            System.out.println("p = " + p + " q = " + q + " r = p%q = " + r);
+            p = q;
+            q = r;
+        }
+        System.out.println("p = " + p + " q = " + q + " r = p%q = " + p % q);
+        return q;
+    }
+}
