9d91f44fcb9feefabdd0cfe01da8d9aafdd8120b
Captain Sir <jakeovski@gmail.com>
2020-05-12 15:11:43 +0300

    Finished Task 6.4

--- CourierDelivery/src/main/java/cm2100/part3/Main.java
+++ CourierDelivery/src/main/java/cm2100/part3/Main.java
@@ -8,9 +8,11 @@ package cm2100.part3;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
+import java.util.TreeSet;
 
 /**
- * Main Class 
+ * Main Class
+ *
  * @author K. Hui // Modified by Vladimir Bardadom 1807132
  */
 public class Main {
@@ -72,63 +74,63 @@ public class Main {
         //********************************************************
         //-----------------------Task 6.3-------------------------
         //********************************************************
-        //List 1
-        //Declaring and initializing ArrayList
+        //-------------------------List 1
+        //-------------------------Declaring and initializing ArrayList
         ArrayList<Driver> drivers = new ArrayList<>();
-        //--------------------------------------------
-        //Drivers
+//        //--------------------------------------------
+//        //--------------------------Drivers
         Driver porter1 = new Driver("Sam Bridges");
         Driver porter2 = new Driver("Fragile");
         Driver porter3 = new Driver("Mads Mikkelsen");
-        //--------------------------------------------
-        //Adding them to ArrayList
+//        //--------------------------------------------
+//        //------------------------Adding them to ArrayList
         drivers.add(porter1);
         drivers.add(porter2);
         drivers.add(porter3);
-        //-----------------------
-        //Printing
-        System.out.println("-------Unsorted-----------------");
-        for (int i = 0; i < drivers.size(); i++) {
-            System.out.println(drivers.get(i));
-        }
+//        //-----------------------
+//        //-------------------------Printing
+//        System.out.println("-------Unsorted-----------------");
+//        for (int i = 0; i < drivers.size(); i++) {
+//            System.out.println(drivers.get(i));
+//        }
         //-------------------------------------------------------
 
-        //List 2
-        //Declaring and initializing LinkedList
+        //----------------------------List 2
+        //----------------------------Declaring and initializing LinkedList
         LinkedList<Driver> drivers2 = new LinkedList<>();
-        //-------------------------------------
-        //Drivers
+//        //-------------------------------------
+//        //----------------------------Drivers
         Driver porter4 = new Driver("Heartman");
         Driver porter5 = new Driver("Guillermo del Toro");
         Driver porter6 = new Driver("Higgs");
-        //-----------------------------------
-        //Adding them to LinkedList
+//        //-----------------------------------
+//        //---------------------------Adding them to LinkedList
         drivers2.add(porter4);
         drivers2.add(porter5);
         drivers2.add(porter6);
-        //-------------------------
-        //Printing
-        System.out.println("Linked List: \n" + drivers2);
-        //-----------------------------------------------
-        //Adding elements from list 2 to list 1
+//        //-------------------------
+//        //-------------------------Printing
+//        System.out.println("Linked List: \n" + drivers2);
+//        //-----------------------------------------------
+//        //----------------------------Adding elements from list 2 to list 1
         for (Driver d : drivers2) {
             drivers.add(d);
         }
         //---------------------------------------
-        //Printing
+        //------------------------------Printing
         System.out.println("-------Unsorted-----------------");
         for (int i = 0; i < drivers.size(); i++) {
             System.out.println(drivers.get(i));
         }
-        //---------------------------------------------------
-        //Sorting the list in alphabetical order then printing the result
+//        //---------------------------------------------------
+//        //-------Sorting the list in alphabetical order then printing the result
         Collections.sort(drivers);
         System.out.println("-------Sorted-----------------");
         for (int i = 0; i < drivers.size(); i++) {
             System.out.println(drivers.get(i));
         }
-        //----------------------------------------------------
-        //Sorting the list in reverse order and printing the results
+//        //----------------------------------------------------
+//        //-------Sorting the list in reverse order and printing the results
         Collections.sort(drivers, Collections.reverseOrder());
         System.out.println("-------Reverse Sorted-----------------");
         for (int i = 0; i < drivers.size(); i++) {
@@ -139,5 +141,43 @@ public class Main {
         //***************************************************************
         //-------------------------6.4-----------------------------------
         //***************************************************************
+        //------------------------Set # 1--------------------------------
+        //--------------------Declaring and initializing a TreeSet
+        TreeSet<Driver> driversSet = new TreeSet<>();
+        //--------------------Adding first 3 Drivers from drivers ArrayList----
+        System.out.println("--------------Set 1---------------");
+        for (int i = 0; i < 3; i++) {
+            driversSet.add(drivers.get(i));
+        }
+        //--------------------Printing
+        System.out.println(driversSet);
+
+        //--------------------Set # 2----------------------------------
+        //--------------------Creating another TreeSet -------------
+        TreeSet<Driver> driversSet2 = new TreeSet<>();
+        //Adding two of the same Drivers that we have added in driversSet
+        for (int i = 0; i < 2; i++) {
+            driversSet2.add(drivers.get(i));
+        }
+        //------------------Adding other two drivers that are not in driversSet
+        driversSet2.add(porter2);
+        driversSet2.add(porter5);
+
+        //------------------Printing
+        System.out.println("---------------Set 2--------------");
+        System.out.println(driversSet2);
+
+        //------------------Intersection of sets--------------------
+        TreeSet<Driver> intersection = new TreeSet<>(driversSet);
+        intersection.retainAll(driversSet2);
+        System.out.println("---------------Intersection of Set 1 and Set 2-------");
+        System.out.println(intersection);
+
+        //-----------------Relative Complement of Sets----------------
+        TreeSet<Driver> relativeComp = new TreeSet<>(driversSet);
+        relativeComp.removeAll(driversSet2);
+        System.out.println("---------------Relative Complement of Set 2 in Set 1----");
+        System.out.println(relativeComp);
+
     } //end method
 } //end class
